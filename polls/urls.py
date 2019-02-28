9ffb798a236abf0c6b2914fe4d0e42da75a9e7d1
bluerain <770120041@qq.com>
2019-02-27 23:56:13 -0600

    finished dynamic url fetching

--- polls/urls.py
+++ polls/urls.py
@@ -6,14 +6,9 @@ from polls.views import TableView
 
 # name is used for a particular mapping(a line in urlpatterns)
 
-"""
-    TODO:
-        passing more variables to url function  
-        https://docs.djangoproject.com/en/1.11/topics/http/urls/#passing-extra-options-to-view-functions
-"""
 
 CSV_PATH = {
-        "weka": "polls\\data\\1_3cweka.csv",
+        "cweka": "polls\\data\\1_3cweka.csv",
         "chinese_stock": "polls\\data\\2_chinese_stock.csv",
         "license_plate": "polls\\data\\3_license_plate.csv",
         "hapiness": "polls\\data\\4_hapiness.csv"
@@ -27,8 +22,9 @@ urlpatterns = [
 
 # adding base tables here
 urlpatterns += [
-    path('table1/', TableView.as_view(table_name="weka"), name='table1'),
-    path('table2/', TableView.as_view(table_name="chinese_stock"), name='table2'),
-    path('table3/', TableView.as_view(table_name="license_plate"), name='table3'),
-    path('table4/', TableView.as_view(table_name="hapiness"), name='table4'),
+    # path('table1/', TableView.as_view(table_name="weka"), name='table1'),
+    # path('table2/', TableView.as_view(table_name="chinese_stock"), name='table2'),
+    # path('table3/', TableView.as_view(table_name="license_plate"), name='table3'),
+    # path('table4/', TableView.as_view(table_name="hapiness"), name='table4'),
+    path(r'table/<slug:slug>', TableView.as_view(), name='table_url_parsing'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -7,21 +7,16 @@ from polls.models import *
 
 
 def home(request):
-    return render(request, 'polls/index.html', {'table':"homepage"})
+    return render(request, 'polls/index.html', {'table': "homepage"})
 
 
 class TableView(View):
-    TABLE_PATH = {
-        1: "polls\\data\\1_3cweka.csv",
-        2: "polls\\data\\2_chinese_stock.csv",
-        3: "polls\\data\\3_license_plate.csv",
-        4: "polls\\data\\4_hapiness.csv"
-    }
-    TEMPLATE_NAME =  'polls/index.html'
+    TEMPLATE_NAME = 'polls/index.html'
     table_name = ""
 
-    def get(self, request):
-        dfmodel = get_object_or_404(DataFrameModel, data_frame_name=self.table_name )
+    def get(self, request, slug):
+        print(slug)
+        dfmodel = get_object_or_404(DataFrameModel, data_frame_name=slug)
         context ={
             "table_name": self.table_name,
             "table": csv_to_html(dfmodel.data_frame_path)
