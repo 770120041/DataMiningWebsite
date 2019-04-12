00e5782dbb5feeba86ec4ba24444ad56f4d5f878
bluerain <770120041@qq.com>
2019-04-12 13:06:35 -0500

    update requirements

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # import this module to serilize dataframez
-# from picklefield.fields import PickledObjectField
 from django.urls import  reverse
 # Create your models here.
 
